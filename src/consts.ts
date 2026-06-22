7e25faca567820dfcad6a28927363d5a44d5b1ff
4rullru <4rullru@gmail.com>
2026-06-22 15:26:37 +0900

    수정: sitename

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = '룰루의 ETF 분석';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
