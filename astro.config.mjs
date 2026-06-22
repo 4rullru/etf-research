497d124308ecd00d6ff68d980e34f129b38abf00
4rullru <4rullru@gmail.com>
2026-06-22 13:31:37 +0900

    Initial commit: Setup Astro blog

---
+++ astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // GitHub Pages 기본 도메인
+  site: 'https://4rullru.github.io',
+  
+  // ⚠️ 확정하신 레포지토리 이름으로 경로 설정 (매우 중요)
+  base: '/etf-research',
+
+  integrations: [mdx(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Atkinson',
+      cssVariable: '--font-atkinson',
+      fallbacks: ['sans-serif'],
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            weight: 400,
+            style: 'normal',
+            display: 'swap',
+          },
+          {
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            weight: 700,
+            style: 'normal',
+            display: 'swap',
+          },
+        ],
+      },
+    },
+  ],
+});
